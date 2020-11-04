bbb10b6557500c45a09f3b879f4a9546dd2f2371
Danil Shvalov <shvalovich@gmail.com>
2020-11-04 19:52:38 +1000

    Добавлена проверка валидации

--- scripts/script.js
+++ scripts/script.js
@@ -2,179 +2,203 @@ const notFound = "images/not-found.svg";
 
 // Конструкторы
 function Form(selector) {
-    this.body = document.querySelector(selector);
-    this.setSubmitHandler = function (handler) {
-        this.body.addEventListener("submit", handler);
-    };
-    this.setProperties = function ({ name, job }) {
-        const { nameInput, jobInput } = this.body;
-        nameInput.value = name;
-        jobInput.value = job;
-    };
+  this.body = document.querySelector(selector);
+  this.setSubmitHandler = function (handler) {
+    this.body.addEventListener("submit", handler);
+    return this;
+  };
+  this.setProperties = function ({ name, job }) {
+    const { nameInput, jobInput } = this.body;
+    nameInput.value = name;
+    jobInput.value = job;
+    return this;
+  };
 }
 
 function Popup(selector) {
-    this.popup = document.querySelector(selector);
-    this.popup.querySelector(".popup__close-button").addEventListener("click", () => this.close());
-    this.popup.addEventListener("click", (evt) => {
-        if (evt.target == this.popup) {
-            this.close();
-        }
-    });
-    this.escape = (evt) => {
-        if (evt.code == "Escape") {
-            this.close();
-        }
-    };
-    this.open = function () {
-        document.addEventListener("keydown", this.escape);
-        this.popup.classList.add("popup_opened");
-    };
-    this.close = function () {
-        document.removeEventListener("keydown" , this.escape);
-        this.popup.classList.remove("popup_opened");
-    };
+  this.popup = document.querySelector(selector);
+  this.popup
+    .querySelector(".popup__close-button")
+    .addEventListener("click", () => this.close());
+  this.popup.addEventListener("click", (evt) => {
+    if (evt.target == this.popup) {
+      this.close();
+      return this;
+    }
+  });
+  this.escape = (evt) => {
+    if (evt.code == "Escape") {
+      this.close();
+    }
+  };
+  this.open = function () {
+    document.addEventListener("keydown", this.escape);
+    this.popup.classList.add("popup_opened");
+    return this;
+  };
+  this.close = function () {
+    document.removeEventListener("keydown", this.escape);
+    this.popup.classList.remove("popup_opened");
+    return this;
+  };
 }
 
 function Element(template) {
-    this.body = template.cloneNode(true);
-    this.title = this.body.querySelector(".element__title");
-    this.image = this.body.querySelector(".element__image");
-    this.likeButton = this.body.querySelector(".element__like-button");
-    this.deleteButton = this.body.querySelector(".element__delete-button");
+  this.body = template.cloneNode(true);
+  this.title = this.body.querySelector(".element__title");
+  this.image = this.body.querySelector(".element__image");
+  this.likeButton = this.body.querySelector(".element__like-button");
+  this.deleteButton = this.body.querySelector(".element__delete-button");
 
-    this.setListener = function (property, type, callback) {
-        this[property].addEventListener(type, callback);
-        return this;
-    };
+  this.setListener = function (property, type, callback) {
+    this[property].addEventListener(type, callback);
+    return this;
+  };
 
-    this.setProperties = function ({ title = "Not found", link = notFound }) {
-        this.image.src = link;
-        this.image.alt = title;
-        this.title.textContent = title;
-        return this;
-    };
+  this.setProperties = function ({ title = "Not found", link = notFound }) {
+    this.image.src = link;
+    this.image.alt = title;
+    this.title.textContent = title;
+    return this;
+  };
 
-    this.render = function (parent) {
-        parent.prepend(this.body);
-        return this;
-    };
+  this.render = function (parent) {
+    parent.prepend(this.body);
+    return this;
+  };
 }
 
 function createElement(data, template, parent, imagePopup) {
-    const element = new Element(template);
-    element.setProperties(data)
-        .setListener("likeButton", "click", (evt) => {
-            evt.target.classList.toggle("button_like-active");
-        }).setListener("deleteButton", "click", (evt) => {
-            evt.target.closest(".element").remove();
-        }).setListener("image", "click", () => {
-            imagePopup.open({ title: element.title.textContent, link: element.image.src });
-        }).setListener("image", "error", function () {
-            this.src = notFound;
-        }).render(parent);
-    return element;
+  const element = new Element(template);
+  element
+    .setProperties(data)
+    .setListener("likeButton", "click", (evt) => {
+      evt.target.classList.toggle("button_like-active");
+    })
+    .setListener("deleteButton", "click", (evt) => {
+      evt.target.closest(".element").remove();
+    })
+    .setListener("image", "click", () => {
+      imagePopup.open({
+        title: element.title.textContent,
+        link: element.image.src,
+      });
+    })
+    .setListener("image", "error", function () {
+      this.src = notFound;
+    })
+    .render(parent);
+  return element;
 }
-
 
 // Переменные
 const initialCards = [
-    {
-        title: "Карелия",
-        link: "https://source.unsplash.com/pf07Opq5Zx4"
-    },
-    {
-        title: "Камчатка",
-        link: "https://source.unsplash.com/Jjs9INF8U8M"
-    },
-    {
-        title: "Судак",
-        link: "https://source.unsplash.com/xwL6wxkzBvk"
-    },
-    {
-        title: "Роза Хутор",
-        link: "https://source.unsplash.com/P7Ij0GY36sA"
-    },
-    {
-        title: "Остров Ольхон",
-        link: "https://source.unsplash.com/y-b_FOmJXdg"
-    },
-    {
-        title: "Карачаевск",
-        link: "https://source.unsplash.com/vrPqM2OB9nA"
-    }
+  {
+    title: "Карелия",
+    link: "https://source.unsplash.com/pf07Opq5Zx4",
+  },
+  {
+    title: "Камчатка",
+    link: "https://source.unsplash.com/Jjs9INF8U8M",
+  },
+  {
+    title: "Судак",
+    link: "https://source.unsplash.com/xwL6wxkzBvk",
+  },
+  {
+    title: "Роза Хутор",
+    link: "https://source.unsplash.com/P7Ij0GY36sA",
+  },
+  {
+    title: "Остров Ольхон",
+    link: "https://source.unsplash.com/y-b_FOmJXdg",
+  },
+  {
+    title: "Карачаевск",
+    link: "https://source.unsplash.com/vrPqM2OB9nA",
+  },
 ];
 
 const editButton = document.querySelector(".profile__edit-button");
 const addButton = document.querySelector(".profile__add-button");
 
-const templateElement = document.querySelector(".template-element").content.querySelector(".element");
+const templateElement = document
+  .querySelector(".template-element")
+  .content.querySelector(".element");
 const elements = document.querySelector(".elements");
 
 const userInfo = {
-    name: document.querySelector(".profile__name"),
-    job: document.querySelector(".profile__about"),
-    setUserInfo({ name, job }) {
-        this.name.textContent = name;
-        this.job.textContent = job;
-    },
-    getUserInfo() {
-        return { name: this.name.textContent.trim(), job: this.job.textContent.trim() };
-    }
+  name: document.querySelector(".profile__name"),
+  job: document.querySelector(".profile__about"),
+  setUserInfo({ name, job }) {
+    this.name.textContent = name;
+    this.job.textContent = job;
+  },
+  getUserInfo() {
+    return {
+      name: this.name.textContent.trim(),
+      job: this.job.textContent.trim(),
+    };
+  },
 };
 
 const editPopup = {
-    popup: new Popup(".popup_edit-profile"),
-    form: new Form(".popup__edit-profile-form"),
-    user: userInfo,
-    open() {
-        this.form.setProperties(this.user.getUserInfo());
-        this.popup.open();
-    },
-    close() {
-        this.popup.close();
-    }
+  popup: new Popup(".popup_edit-profile"),
+  form: new Form(".popup__edit-profile-form").setProperties(userInfo),
+  user: userInfo,
+  open() {
+    this.form.setProperties(this.user.getUserInfo());
+    this.popup.open();
+  },
+  close() {
+    this.popup.close();
+  },
 };
 
 const addPopup = {
-    popup: new Popup(".popup_add-element"),
-    form: new Form(".popup__add-element-form"),
-    open() {
-        this.popup.open();
-    },
-    close() {
-        this.form.body.reset();
-        this.popup.close();
-    }
+  popup: new Popup(".popup_add-element"),
+  form: new Form(".popup__add-element-form"),
+  open() {
+    this.popup.open();
+  },
+  close() {
+    this.form.body.reset();
+    this.popup.close();
+  },
 };
 
 const imagePopup = {
-    popup: new Popup(".popup_image-block"),
-    image: document.querySelector(".popup__image"),
-    description: document.querySelector(".popup__image-description"),
-    open({ title, link }) {
-        this.image.src = link;
-        this.image.alt = title;
-        this.description.textContent = title;
-        this.popup.open();
-    }
+  popup: new Popup(".popup_image-block"),
+  image: document.querySelector(".popup__image"),
+  description: document.querySelector(".popup__image-description"),
+  open({ title, link }) {
+    this.image.src = link;
+    this.image.alt = title;
+    this.description.textContent = title;
+    this.popup.open();
+  },
 };
 
 // Listeners
 editPopup.form.setSubmitHandler(function (evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    const { nameInput, jobInput } = editPopup.form.body;
-    editPopup.user.setUserInfo({ name: nameInput.value, job: jobInput.value });
-    editPopup.close();
+  const { nameInput, jobInput } = editPopup.form.body;
+  editPopup.user.setUserInfo({ name: nameInput.value, job: jobInput.value });
+  editPopup.close();
 });
 
 addPopup.form.setSubmitHandler(function (evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    const { titleInput, linkInput } = addPopup.form.body;
-    createElement({ title: titleInput.value, link: linkInput.value }, templateElement, elements, imagePopup);
-    addPopup.close();
+  const { titleInput, linkInput } = addPopup.form.body;
+  createElement(
+    { title: titleInput.value, link: linkInput.value },
+    templateElement,
+    elements,
+    imagePopup
+  );
+  addPopup.close();
 });
 
 editButton.addEventListener("click", () => editPopup.open());
@@ -182,7 +206,5 @@ addButton.addEventListener("click", () => addPopup.open());
 
 // Инициализация карточке
 initialCards.forEach((data) => {
-    createElement(data, templateElement, elements, imagePopup);
+  createElement(data, templateElement, elements, imagePopup);
 });
-
-
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,7 +1,62 @@
-function enableValidation({ formSelector, inputSelector, submitButtonSelector,
-    inactiveButtonClass, inputErrorClass, errorClass }) {
+const selectorsData = {
+  formSelector: ".form",
+  inputSelector: ".input",
+  submitButtonSelector: ".button_type_submit",
+  inactiveButtonClass: "button_type_submit-disabled",
+  inputErrorClass: "input_error",
+  errorClass: "field-error_visible",
+};
 
+function validate() {
+  if (this.validity.valid) {
+    this.classList.remove(this.inputErrorClass);
+    this.errorSpan.classList.remove(this.errorClass);
+    this.errorSpan.textContent = "";
+    if (this.parent.every((input) => input.validity.valid)) {
+      this.submitButton.classList.remove(this.inactiveButtonClass);
+    }
+  } else {
+    this.classList.add(this.inputErrorClass);
+    this.errorSpan.classList.add(this.errorClass);
+    this.errorSpan.textContent = this.validationMessage;
+    this.submitButton.classList.add(this.inactiveButtonClass);
+  }
 }
 
 
+function enableValidation(data) {
+  const forms = getElements(data);
 
+  forms.forEach((form) => {
+    const inputs = modifyInputs(form, data);
+    inputs.forEach((input) => {
+      if (!input.validity.valid) {
+        input.submitButton.classList.add(input.inactiveButtonClass);
+      }
+      input.addEventListener("input", validate);
+    });
+  });
+}
+
+function getElements({ formSelector, inputSelector, submitButtonSelector }) {
+  const forms = Array.from(document.querySelectorAll(formSelector));
+  return forms.map((form) => {
+    const submitButton = form.querySelector(submitButtonSelector);
+    const inputs = Array.from(form.querySelectorAll(inputSelector));
+    return { form, inputs, submitButton };
+  });
+}
+
+function modifyInputs({ inputs, submitButton }, { inactiveButtonClass, inputErrorClass, errorClass }) {
+  return inputs.map((input) => {
+    input.submitButton = submitButton;
+    input.inactiveButtonClass = inactiveButtonClass;
+    input.inputErrorClass = inputErrorClass;
+    input.errorSpan = input.nextElementSibling;
+    input.errorClass = errorClass;
+    input.parent = inputs;
+    return input;
+  });
+}
+
+enableValidation(selectorsData);
